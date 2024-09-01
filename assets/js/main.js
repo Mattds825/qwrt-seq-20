cb0d304cfa4cd511089403f0275a6ec600d14050
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-09-01 21:39:50 +0100

    change current pad text on keypress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const currentPadText = document.getElementById("current-pad-text");
+
 const padKeyMap = {
     1: 1,
     2: 2,
@@ -17,10 +19,33 @@ const padKeyMap = {
     "v": 16,
   };
 
+  const padNameMap = {
+    1: "Synth 1",
+    2: "Synth 2",
+    3: "Synth 3",
+    4: "Synth 4",
+    5: "Bass 1",
+    6: "Bass 2",
+    7: "Bass 3",
+    8: "Bass 4",
+    9: "FX 1",
+    10: "FX 2",
+    11: "FX 3",
+    12: "FX 4",
+    13: "Drums 1",
+    14: "Drums 2",
+    15: "Drums 3",
+    16: "Drums 4",
+  };
+
 function updateValue(slider) {
     if (slider.id === 'vol-slider') document.getElementById('slider-value-vol').textContent = slider.value;
     if(slider.id === 'rev-slider') document.getElementById('slider-value-rev').textContent = slider.value;
     if(slider.id === 'dist-slider') document.getElementById('slider-value-dist').textContent = slider.value;
+}
+
+const setCurrentPadText = (pad) => {
+    currentPadText.textContent = padNameMap[pad];
 }
 
 // keyboard event listener
@@ -30,6 +55,9 @@ document.addEventListener('keydown', (event) => {
     if (padKey !== undefined) {
         const pad = document.getElementById(`pad-${padKey}`);
         pad.classList.add('active');
+
+        // set current pad text
+        setCurrentPadText(padKey);
     }
 });
 
